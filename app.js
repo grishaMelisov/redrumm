21862d057567816ebb137e536f8d2a1588444b75
redrum <urecherrydream@gmail.com>
2024-01-16 14:50:48 -0300

    [fix] fix js script

--- app.js
+++ app.js
@@ -49,5 +49,3 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener('click', hideMobileMenu)
 navLogo.addEventListener('click', hideMobileMenu)
-
-<meta property="og:image" content="http://forms.gmelisov.amocrm2.saas/forms/img/bg_og/defaultbg.png">
